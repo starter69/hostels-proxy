a3ae5702fdfc8d0f9be3e2f5aeb0f1a8519741f9
sean madhavan <sean.madhavan@gmail.com>
2016-09-14 20:40:58 +0800

    update proxy server

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
+var https = require('https');
 
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
-
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  proxy.web(req, res, { target: 'https://www.hostelspoint.com' });
-});
-
-console.log("listening on port 5050")
-server.listen(5050);
+httpProxy.createProxyServer({
+  target: 'https://www.hotelspoint.com',
+  agent  : https.globalAgent,
+  headers: {
+    host: 'hostelspoint.com'
+  }
+}).listen(5050);
