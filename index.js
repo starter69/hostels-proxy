ca3833904949c6be135d0e6cab7c8a3e24fbc957
sean madhavan <sean.madhavan@gmail.com>
2016-09-14 20:56:31 +0800

    handling post on proxy

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ var server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
   console.log(req.headers);
+  proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    if(req.method=="POST" && req.body){
+        proxyReq.write(req.body);
+        proxyReq.end();
+    }
+  });
   proxy.web(req, res, { target: 'https://www.hostelspoint.com' });
 });
 
